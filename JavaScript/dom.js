cef4d71c2c03d97169cc0a33c65d1c9acc1cd2a9
K Mohan <km830719@gmail.com>
2023-05-18 05:31:16 +0530

    added querSelector

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -50,16 +50,61 @@
 
 //getElementsByTagName  //it also returns the htmlCollection
 
-var li=document.getElementsByTagName('li');
-console.log(li)
-console.log(li[1]);
-li[1].textContent="Hello"
-li[1].style.fontWeigth='bold';
-li[2].style.backgroundColor='green';
+// var li=document.getElementsByTagName('li');
+// console.log(li)
+// console.log(li[1]);
+// li[1].textContent="Hello"
+// li[1].style.fontWeigth='bold';
+// li[2].style.backgroundColor='green';
 
-//gives error as getElementsByClassName return htmlCollection
-//items.style.backgroundColor='#f4f4f4';
+// //gives error as getElementsByClassName return htmlCollection
+// //items.style.backgroundColor='#f4f4f4';
 
-for(let i=0;i<li.length;i++){
-    li[i].style.color="red";
+// for(let i=0;i<li.length;i++){
+//     li[i].style.color="red";
+// }
+
+//QUERYSELECTOR// it will select only the first occuring element
+
+var header=document.querySelector('#main-header');
+console.log(header);
+header.style.borderBottom='2px solid black';
+
+var input=document.querySelector('input');
+input.value="hello world";
+
+var submit= document.querySelector('input[type="submit"]');
+submit.value='send';
+
+var item=document.querySelector('.list-group-item');
+item.style.color="blue"  //only first item gets color
+
+var lastItem=document.querySelector('.list-group-item:last-child');
+lastItem.style.color="pink"
+
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor="green"
+
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display="none";
+
+//QUERYSELECTORALL// it will select multiple elements
+
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[1].textContent='hello'
+
+var secondItem=document.querySelectorAll('.list-group-item')
+console.log(secondItem)
+secondItem[1].style.color="green";
+
+var odd=document.querySelectorAll("li:nth-child(odd)");
+var even=document.querySelectorAll("li:nth-child(even)");
+
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#ccc'
+    even[i].style.backgroundColor='#f4f4f4'
 }
+
+
+
