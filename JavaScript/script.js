3101c27bc3ad98c79b2dcf872c4d18f97dc13105
K Mohan <km830719@gmail.com>
2023-05-19 10:58:44 +0530

    added delete button

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,5 +17,26 @@ document.getElementById("submitBtn").addEventListener("click", function(event) {
   let myobj_serialized=JSON.stringify(myobj);
 
   localStorage.setItem(email,myobj_serialized );
+
+  // Create a new <li> element
+  var li = document.createElement("li");
+  li.textContent = "Name: " + name + ", Email: " + email + ", Phone: " + phone ;
+  //adding a delete button
+  var deleteBtn=document.createElement('button');
+  deleteBtn.id='delete';
+  deleteBtn.textContent='delete';
+  li.appendChild(deleteBtn);
+
+  // Append the <li> element to an existing <ul> element on the screen
+  var ul = document.getElementById("userList");
+  ul.appendChild(li);
+
+deleteBtn.addEventListener("click",function(){
+  deleteUser(email);
+})
  
 });
+
+function deleteUser(email){
+  localStorage.removeItem(email);
+}
