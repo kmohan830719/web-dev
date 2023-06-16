009b857a6fdd4b88b8e5037f780731b0b951209d
K Mohan <km830719@gmail.com>
2023-06-16 08:59:30 +0530

    put request

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,27 +16,27 @@ document.getElementById("submitBtn").addEventListener("click", function(event) {
   }
 
   //doing a post request to crudcrud
-  axios.post("https://crudcrud.com/api/4cfbba6ebe4242388a76a75dc00fbee5/appointment", myobj)
-  .then((responce)=>{
-    // console.log(responce);
-    showDetails(responce.data);
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
+  // axios.post("https://crudcrud.com/api/6a59010557b3430aa461e6ab08b2c698/appointment", myobj)
+  // .then((responce)=>{
+  //   // console.log(responce);
+  //   showDetails(responce.data);
+  // })
+  // .catch((err)=>{
+  //   console.log(err);
+  // })
 
   //storing in local storage
-  // let myobj_serialized=JSON.stringify(myobj);
-  // localStorage.setItem(email,myobj_serialized );
+  let myobj_serialized=JSON.stringify(myobj);
+  localStorage.setItem(email,myobj_serialized );
 
-  //showing on the screen
+  //showing on the screen from localstorage
  //showDetails(myobj);
  
 });
 
  //doing a get request
  window.addEventListener("DOMContentLoaded",()=>{
-  axios.get("https://crudcrud.com/api/4cfbba6ebe4242388a76a75dc00fbee5/appointment")
+  axios.get("https://crudcrud.com/api/6a59010557b3430aa461e6ab08b2c698/appointment")
   .then((response)=>{
     console.log(response);
     for(var i=0;i<response.data.length;i++){
@@ -75,7 +75,7 @@ function showDetails(obj){
    //to delete user from crudcrud as well as localstorage
    deleteBtn.addEventListener("click",function(e){
     e.preventDefault();
-    axios.delete("https://crudcrud.com/api/4cfbba6ebe4242388a76a75dc00fbee5/appointment/648b72dbc632b703e83047a6")
+    axios.delete(`https://crudcrud.com/api/6a59010557b3430aa461e6ab08b2c698/appointment/${obj._id}`)
     .then((promis)=>{
       console.log(promis)
     })
@@ -88,19 +88,46 @@ function showDetails(obj){
   
   editBtn.addEventListener("click",function(e){
     e.preventDefault();
-    var userData = JSON.parse(localStorage.getItem(obj.Email));
+    axios.get(`https://crudcrud.com/api/6a59010557b3430aa461e6ab08b2c698/appointment/${obj._id}`)
+    .then((promis)=>{
+      console.log(promis.data)
+      if (promis) {
+        document.getElementById("myname").value = promis.data.Name;
+        document.getElementById("myemail").value = promis.data.Email;
+        document.getElementById("myphone").value = promis.data.Phone_no;
+        document.getElementById("mydate").value = promis.data.Date;
+        document.getElementById("mytime").value = promis.data.Time;
+      }
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+    // var userData = JSON.parse(localStorage.getItem(obj.Email));
   
-    if (userData) {
-      document.getElementById("myname").value = userData.Name;
-      document.getElementById("myemail").value = userData.Email;
-      document.getElementById("myphone").value = userData.Phone_no;
-      document.getElementById("mydate").value = userData.Date;
-      document.getElementById("mytime").value = userData.Time;
-    }
-    deleteUser(obj.Email);
+    // deleteUser(obj.Email);
   
      // Remove the user from the UI
      li.remove();
   })
+  document.getElementById("submitBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var updatedData = {
+      Name: document.getElementById("myname").value,
+      Email: document.getElementById("myemail").value,
+      Phone_no: document.getElementById("myphone").value,
+      Date: document.getElementById("mydate").value,
+      Time: document.getElementById("mytime").value
+    };
+
+    axios.put(`https://crudcrud.com/api/6a59010557b3430aa461e6ab08b2c698/appointment/${obj._id}`, updatedData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    })
 }
  
